ee925e93e2479a09e8642dfa3898016eeaad2c0b
Mahmut İsmail Öztürk <mahmutismail83@gmail.com>
2026-04-08 10:52:32 +0300

    Lab 01

---
+++ lab01/src/tr/edu/mu/se3006/Main.java
@@ -0,0 +1,24 @@
+package tr.edu.mu.se3006;
+import tr.edu.mu.se3006.presentation.OrderController;
+import tr.edu.mu.se3006.business.OrderService;
+import tr.edu.mu.se3006.persistence.ProductRepository;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🚀 System Starting...\n");
+        
+        // TODO 1: Create the lowest layer (ProductRepository)
+        final ProductRepository productRepository = new ProductRepository();
+        
+        // TODO 2: Create the middle layer (OrderService) and inject the repository
+        final OrderService orderService = new OrderService(productRepository);
+        
+        // TODO 3: Create the top layer (OrderController) and inject the service
+        final OrderController controller = new OrderController(orderService);
+        
+        System.out.println("--- Test Scenarios ---");
+        // TODO 4: Call handleUserRequest via the controller to test the system
+        // e.g., controller.handleUserRequest(1L, 2);
+        controller.handleUserRequest(1L, 2);
+        controller.handleUserRequest(2L, 5);}
+}
---
+++ lab01/src/tr/edu/mu/se3006/presentation/OrderController.java
@@ -0,0 +1,26 @@
+package tr.edu.mu.se3006.presentation;
+import java.util.HashMap;
+
+import tr.edu.mu.se3006.business.OrderService;
+
+public class OrderController {
+    // TODO: Define OrderService dependency and use Constructor Injection
+    public OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    public void handleUserRequest(Long productId, int quantity) {
+        System.out.println(">>> New Request: Product ID=" + productId + ", Quantity=" + quantity);
+        // TODO: Call placeOrder inside a try-catch block. 
+        try {
+            orderService.placeOrder(productId, quantity);
+            System.out.println("✅ Order Confirmed");
+
+        } catch (Exception e) {
+            System.out.println("❌ ERROR: " + e.getMessage());
+        }
+        // Print "✅ Order Confirmed" on success, or "❌ ERROR: [message]" on failure.
+    }
+}
